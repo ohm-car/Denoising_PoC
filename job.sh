b84ac9ba12699702e505b4200392f5e719b3d0ae
ohm-car <omk.ubu1604@gmail.com>
2026-03-27 20:59:47 -0400

    ddp trial

--- job.sh
+++ job.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=bash
+#SBATCH --job-name=ddpm
 #SBATCH --mail-user=omkark1@umbc.edu
-#SBATCH --mem=48G
-#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:2
 #SBATCH --time=20-12:00:00
 #SBATCH --constraint=L40S
-#SBATCH --output=diffout.out
-#SBATCH --error=diffout.err
+#SBATCH --output=diffout_%j.out
+#SBATCH --error=diffout_%j.err
 
 # uv run torchrun evaluate_denoised.py
-uv run torchrun --master-port=29501 evaluate_denoised.py
+# uv run torchrun --master-port=29501 evaluate_denoised.py
+uv run torchrun --master-port=29501 --nproc_per_node=2 train_ddp.py
