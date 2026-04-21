e4c2c6ad5c613d69d80911e13c6a18c6a1aa65ed
ohm-car <omk.ubu1604@gmail.com>
2026-04-21 17:08:17 -0400

    fixing bugs

--- datasets/nih_dataset.py
+++ datasets/nih_dataset.py
@@ -1,60 +1,5 @@
-# import torch
-# import pandas as pd
-# import os
-# from torch.utils.data import Dataset, DataLoader
-# from PIL import Image
-# import torchvision.transforms as T
-
-# class NIHDataset(Dataset):
-#     def __init__(self, split='train', resolution=1024, data_path="./data"):
-#         self.res = resolution
-#         self.pathologies = [
-#             "Atelectasis", "Cardiomegaly", "Effusion", "Infiltration", 
-#             "Mass", "Nodule", "Pneumonia", "Pneumothorax", 
-#             "Consolidation", "Edema", "Emphysema", "Fibrosis", 
-#             "Pleural_Thickening", "Hernia"
-#         ]
-        
-#         df = pd.read_csv(os.path.join(data_path, "Data_Entry_2017.csv"))
-        
-#         # Filter based on official .txt files
-#         if split in ['train', 'val']:
-#             with open(os.path.join(data_path, "train_val_list.txt"), 'r') as f:
-#                 split_list = [line.strip() for line in f.readlines()]
-#             df = df[df['Image Index'].isin(split_list)]
-            
-#             # Internal 90/10 split for training vs validation
-#             train_df = df.sample(frac=0.9, random_state=42)
-#             self.df = train_df if split == 'train' else df.drop(train_df.index)
-#         else:
-#             with open(os.path.join(data_path, "test_list.txt"), 'r') as f:
-#                 split_list = [line.strip() for line in f.readlines()]
-#             self.df = df[df['Image Index'].isin(split_list)]
-
-#         self.transform = T.Compose([
-#             T.Resize((self.res, self.res)),
-#             T.Grayscale(),
-#             T.ToTensor(),
-#             T.Normalize(mean=[0.5], std=[0.5]) 
-#         ])
-
-#     def __len__(self):
-#         return len(self.df)
-
-#     def __getitem__(self, idx):
-#         row = self.df.iloc[idx]
-#         img_path = os.path.join("./images", row['Image Index'])
-#         image = Image.open(img_path).convert("L")
-#         return self.transform(image), torch.tensor(row[self.pathologies].values.astype(float))
-
-# def get_nih_loaders(batch_size=4, res=1024, data_path="./data"):
-#     train_loader = DataLoader(NIHDataset('train', res, data_path), batch_size=batch_size, shuffle=True, num_workers=8)
-#     val_loader = DataLoader(NIHDataset('val', res, data_path), batch_size=batch_size, shuffle=False, num_workers=8)
-#     test_loader = DataLoader(NIHDataset('test', res, data_path), batch_size=batch_size, shuffle=False, num_workers=8)
-#     return train_loader, val_loader, test_loader
-
-
 import os
+import sys
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -84,21 +29,17 @@ class NIHDataset(Dataset):
         try:
             image = Image.open(img_path).convert('L')
         except Exception as e:
-            print(f"Error loading {img_path}: {e}")
-            image = Image.new('L', (1024, 1024))
+            print(f"CRITICAL ERROR: Image not found at {img_path}.")
+            print("Please verify your IMG_DIR path and ensure the dataset is fully extracted.")
+            sys.exit(1)
         
         if self.transform:
             image = self.transform(image)
 
-        if not isinstance(image, torch.Tensor):
-            image = transforms.functional.to_tensor(image)
-
-        image = image.to(torch.float32)
-
-        # Min-Max Normalize to [0, 1]
-        i_min, i_max = image.min(), image.max()
-        if i_max > i_min:
-            image = (image - i_min) / (i_max - i_min)
+        # # Min-Max Normalize to [0, 1]
+        # i_min, i_max = image.min(), image.max()
+        # if i_max > i_min:
+        #     image = (image - i_min) / (i_max - i_min)
 
         # Clinical Scaling for XRV compatibility [-1024, 1024]
         image = (image * 2048.0) - 1024.0
@@ -136,9 +77,20 @@ def get_nih_loaders(csv_path, img_dir, batch_size=16, resize_to=1024, test_size=
         v2.ToDtype(torch.float32, scale=True), 
     ])
 
-    train_loader = DataLoader(NIHDataset(train_df, img_dir, transform), batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
-    val_loader = DataLoader(NIHDataset(val_df, img_dir, transform), batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
-    test_loader = DataLoader(NIHDataset(test_df, img_dir, transform), batch_size=batch_size, shuffle=False, num_workers=1, pin_memory=True)
+    # Calculate optimal num_workers
+    # os.cpu_count() respects Slurm's --cpus-per-task allocation
+    total_cores = os.cpu_count()
+    world_size = dist.get_world_size() # How many GPUs are running
+    
+    # Give each GPU half of its fair share of CPU cores (minimum 1)
+    optimal_workers = max(1, (total_cores // world_size) // 2)
+    
+    if local_rank == 0:
+        print(f"Dynamically set num_workers to: {optimal_workers} per GPU")
+
+    train_loader = DataLoader(NIHDataset(train_df, img_dir, transform), batch_size=batch_size, shuffle=True, num_workers=optimal_workers, pin_memory=True)
+    val_loader = DataLoader(NIHDataset(val_df, img_dir, transform), batch_size=batch_size, shuffle=False, num_workers=optimal_workers, pin_memory=True)
+    test_loader = DataLoader(NIHDataset(test_df, img_dir, transform), batch_size=batch_size, shuffle=False, num_workers=optimal_workers, pin_memory=True)
 
     loaders = {
         'train': train_loader,
