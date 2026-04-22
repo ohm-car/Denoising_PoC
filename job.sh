51e699dd08ca61cf5a2cc66404f4cb86c1dc039f
ohm-car <omk.ubu1604@gmail.com>
2026-04-22 15:18:10 -0400

    bugfixing

--- job.sh
+++ job.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=ddpm
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=64G
-#SBATCH --gres=gpu:8
-#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=4
 #SBATCH --time=20-12:00:00
-#SBATCH --constraint=RTX_6000
+#SBATCH --constraint=L40S
 #SBATCH --output=diffout_%j.out
 #SBATCH --error=diffout_%j.err
 #SBATCH --no-requeue
@@ -14,4 +14,5 @@
 
 # uv run torchrun evaluate_denoised.py
 # uv run torchrun --master-port=29501 evaluate_denoised.py
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 uv run torchrun --master-port=29501 --nproc_per_node=gpu train_ddp.py -j=$SLURM_JOB_ID
